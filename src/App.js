693f38da32575c11fdb6f75cfcae1e50bd5ad1a2
Laura Santos <alsr16p@gmail.com>
2021-06-24 16:34:03 +0100

    started the weather card

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from 'react';
+import Weather from './components/weather';
 
 
 function App() {
 //Create state for latitude and for longitude
 const [lat, setLat ] = useState([]);
 const [long, setLong] = useState([]);
+const [data, setData] = useState([]);
 
 //fuction so that when app is loading and reloading, useEffect
 useEffect(() => {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    setLat(position.coords.latitude);
-    setLong(position.coords.longitude);
-  });
+  const fetchData = async () => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
 
-  console.log("Latitude is:", lat);
-  console.log("Longitude is:", long);
-}, [lat, long]); 
-
+      await
+    fetch(`${process.env.REACT_APP_API_URL}/weather/?
+    lat=${lat}&long=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+    .then(res => res.json())
+    .then(result => {
+      setData(result)
+      console.log(result);
+    });  
+    }
+    fetchData(); 
+  },[lat, long]); 
 
   return (
     <div className="App">
-      
+      {(typeof data.main != 'undefined') ? (
+        <Weather />
+      ): (
+        <div></div>
+      )}
       
     </div>
   );
