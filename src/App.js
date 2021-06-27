bd358d6db12788d173a6b7f00b5e6ca3069f60a7
Laura Santos <alsr16p@gmail.com>
2021-06-27 16:08:37 +0100

    done

--- src/App.js
+++ src/App.js
@@ -11,32 +11,32 @@ const [data, setData] = useState([]);
 
 //fuction so that when app is loading and reloading, useEffect
 useEffect(() => {
+
   const fetchData = async () => {
+
     navigator.geolocation.getCurrentPosition(function(position) {
         setLat(position.coords.latitude);
         setLong(position.coords.longitude);
       });
 
-      await
-    fetch(`${process.env.REACT_APP_API_URL}/weather/?
-    lat=${lat}&long=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-    .then(res => res.json())
-    .then(result => {
-      setData(result)
-      console.log(result);
-    });  
+      await 
+      fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });  
     }
     fetchData(); 
-  },[lat, long]); 
+  }, [lat, long]); 
 
   return (
     <div className="App">
       {(typeof data.main != 'undefined') ? (
-        <Weather />
+        <Weather weatherData={data} />
       ): (
         <div></div>
       )}
-      
     </div>
   );
 }
